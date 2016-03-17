f60d608245f3d2334d14612813a949eb5c378a08
Rodrigo Troncoso <ratroncoso@uc.cl>
2016-03-17 17:25:45 -0300

    asdsa

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import os
 from flask import Flask, request, jsonify, make_response
 import hashlib
-from urllib.request import urlopen
 
 app = Flask(__name__)
 
@@ -30,14 +29,6 @@ def validar():
 def stat():
 	return  make_response(" ", 201)
 
-@app.route('/texto', methods=['GET'])
-def text():
-    texto = urlopen("https://s3.amazonaws.com/files.principal/texto.txt").read()
-    hText = texto.encode("utf-8")
-    m = hashlib.sha256()
-    m.update(hText)
-    hashr = m.hexdigest()
-    return jsonify({'mensaje': texto ,'hash' : hashr})
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT',5000))
