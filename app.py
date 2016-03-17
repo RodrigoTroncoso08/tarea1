aaccb8e337c4fbc45476d4478a510e537398d694
Rodrigo Troncoso <ratroncoso@uc.cl>
2016-03-16 23:00:05 -0300

    asd

--- app.py
+++ app.py
@@ -7,6 +7,20 @@ app = Flask(__name__)
 def hello():
     return 'Hello World'
 
+@app.route('/validarFirma', methods=['POST'])
+def validar():
+    valid = False
+    message = request.form['mensaje']
+    hashU = request.form['hash']
+
+    m = hashlib.sha256()
+    m.update(message)
+    hashr = m.Hexdigest
+    if hashr==hashU:
+        valid=True
+
+    return jsonify( mensaje=message, valido = valid)
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT',5000))
     app.run(host='0.0.0.0', port=port)
