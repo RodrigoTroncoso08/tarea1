08371343e5ee34b62c2b3e6298a9696ec361310c
Rodrigo Troncoso <ratroncoso@uc.cl>
2016-03-17 15:39:06 -0300

    fin

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, request, jsonify, make_response
 import hashlib
 import types
+import urllib.request
 
 app = Flask(__name__)
 
@@ -24,11 +25,20 @@ def validar():
     if hashr==hashU.lower():
         valid=True
 
-    return jsonify({'mensaje': message ,'valido' : valid , 'hashU' : hashU,'hashr' : hashr})
+    return jsonify({'mensaje': message ,'valido' : valid})
 
 @app.route('/status', methods=['GET'])
 def stat():
 	return  make_response(" ", 201)
+
+@app.route('/texto', methods=['GET'])
+def text():
+	texto = urllib.request.urlopen("https://s3.amazonaws.com/files.principal/texto.txt").read()
+	hText = texto.encode("utf-8")
+	m = hashlib.sha256()
+    m.update(hText)
+    hashr = m.hexdigest()
+    return jsonify({'mensaje': texto ,'hash' : hashr})
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT',5000))
